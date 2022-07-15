79b6f4c9adccf11f7591d12ba08567721bbb14c0
Benson Jing <bensonjing23@gmail.com>
2022-07-14 22:25:35 -0400

    dev: add displayGameBoard function

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const gameBoard = (() => {
-  const gameBoardArray = [];
-  return {};
+  const _gameBoardArray = [
+    ["x", "o", "x"],
+    ["x", "x", "o"],
+    ["o", "o", "x"],
+  ];
+
+  const getGameBoard = () => {
+    return _gameBoardArray;
+  };
+  return { getGameBoard };
 })();
 
 const gameController = (() => {
@@ -9,4 +17,13 @@ const gameController = (() => {
 
 const player = () => {
   return {};
+};
+
+displayGameBoard = (gameBoardArray) => {
+  const cells = document.querySelectorAll(".cell");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      cells[j + i * 3].innerHTML = gameBoardArray[i][j];
+    }
+  }
 };
