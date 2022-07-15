4dbb3fe4f224bce8068cdb3680e4c1324254f553
Benson Jing <bensonjing23@gmail.com>
2022-07-14 23:39:48 -0400

    dev: enable player to make moves

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
+const cells = document.querySelectorAll(".cell");
+
 const gameBoard = (() => {
   const _gameBoardArray = [
-    ["x", "o", "x"],
-    ["x", "x", "o"],
-    ["o", "o", "x"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
   const getGameBoard = () => {
     return _gameBoardArray;
   };
-  return { getGameBoard };
+
+  // @param {int} pos: two digit number corresponds to the id of cells
+  const setCell = (pos, player) => {
+    _gameBoardArray[pos[0]][pos[1]] = player.getSign();
+  };
+
+  return { getGameBoard, setCell };
 })();
 
-const gameController = (() => {
-  return {};
-})();
-
-const player = () => {
-  return {};
+const player = (sign) => {
+  const getSign = () => {
+    return sign;
+  };
+  return { getSign };
 };
 
+const gameController = (() => {
+  let currPlayer = player("x");
+
+  const makeMove = (pos) => {
+    gameBoard.setCell(pos, currPlayer);
+    displayGameBoard(gameBoard.getGameBoard());
+  };
+  return { makeMove };
+})();
+
 displayGameBoard = (gameBoardArray) => {
-  const cells = document.querySelectorAll(".cell");
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
       cells[j + i * 3].innerHTML = gameBoardArray[i][j];
     }
   }
 };
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", (e) => {
+    gameController.makeMove(e.target.id);
+  });
+});
